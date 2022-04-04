5619c4ad7ead8cc3f3a04f14e2423ec8ca119c7c
shijian <2954700422@qq.com>
2022-04-04 19:29:13 +0800

    完成可以重复不停猜测，直到猜对为止功能

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -2,21 +2,24 @@ import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         // 1.生成答案数字
         int answer = (int) (Math.random() * 100 + 1);
         // 2.接受猜测数字
-        Scanner scan = new Scanner(System.in);
-        System.out.print("请输入1-100之间的整数:");
-        int guess = scan.nextInt();
-        System.out.println("你输入的是：" + guess);
-        // 3.比较大小
-        if (answer > guess) {
-            System.out.println("你猜小了");
-        } else if (answer < guess) {
-            System.out.println("你猜大了");
-        } else {
-            System.out.println("你猜对了");
-        }
         // 4.每接受一个数字都比较一次大小
+        while(true) {
+            System.out.print("请输入1-100之间的整数:");
+            int guess = scan.nextInt();
+            System.out.println("你输入的是：" + guess);
+            // 3.比较大小
+            if (answer > guess) {
+                System.out.println("你猜小了");
+            } else if (answer < guess) {
+                System.out.println("你猜大了");
+            } else {
+                System.out.println("你猜对了");
+                break;
+            }
+        }
     }
 }
