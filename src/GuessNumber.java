81c41d96c2d6c575f52ba8f33feee4610e06a84b
shijian <2954700422@qq.com>
2022-04-11 18:39:34 +0800

    完成限制次数为7次的功能

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -5,9 +5,12 @@ public class GuessNumber {
         Scanner scan = new Scanner(System.in);
         // 1.生成答案数字
         int answer = (int) (Math.random() * 100 + 1);
+        // 只能猜7次
+        int chances = 7;
         // 2.接受猜测数字
         // 4.每接受一个数字都比较一次大小
-        while(true) {
+        while(chances > 0) {
+            System.out.println("你还有" + chances + "次机会");
             System.out.print("请输入1-100之间的整数:");
             int guess = scan.nextInt();
             System.out.println("你输入的是：" + guess);
@@ -20,6 +23,10 @@ public class GuessNumber {
                 System.out.println("你猜对了");
                 break;
             }
+            chances --;
+        }
+        if (chances == 0) {
+            System.out.println("你没有机会了");
         }
     }
 }
